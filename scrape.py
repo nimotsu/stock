f06e9975788c7751517cdca23c3ee7a8a7a8b632
Xun Wei Yee <hectoryeeos@gmail.com>
2020-08-19 12:27:14 +0800

    Separate classes to stock.py

--- scrape.py
+++ scrape.py
@@ -2,12 +2,11 @@
 # coding: utf-8
 
 import sys
-import requests
-import pandas as pd
-import re
-from bs4 import BeautifulSoup
 import xlsxwriter
 import datetime
+
+from stock import Stock
+from stock import Webpage
 
 import os
 import numpy as np
@@ -17,221 +16,18 @@ warnings.filterwarnings("ignore")
 now = datetime.datetime.now()
 
 
-def url2html(url, headers=None, params=None, data=None):
-    headers={'User-Agent': 'Mozilla/5.0'}
-    try:
-        req = requests.get(url, headers=headers, params=params, data=data)
-    except:
-        req = requests.get(url, headers=headers, params=params, data=data, verify=False)
-    html = req.text
-    return html
+
     
 def search(term: str, df, index = 1):
     return float(df[df[0].str.contains('(?i)' + term)][index].values[0].replace(",", ""))
 
 
-# Handle all urls and htmls
-class Webpage:
-    def __init__(self, html):
-        self.html = html
-        self.soup = BeautifulSoup(self.html, 'html.parser')
-        try:
-            self.tables = pd.read_html(self.html)
-        except:
-            self.tables = None
-    
-    @classmethod
-    def from_url(cls, url, headers=None, params=None, data=None):
-        """constructor with url"""
-
-        html = url2html(url, headers)
-        return cls(html)
-    
-    def get_span(self, tag: str, class_name: list):
-        """return df from columns not in <table>"""
-        
-        def get_tag(tag, class_name):
-            tags = self.soup.find_all(tag, {'class': class_name})
-            text = [i.get_text() for i in tags if i.get_text() != '']
-            return text
-        
-        attrib = get_tag(tag, class_name[0])
-        data = get_tag(tag, class_name[1])
-
-        ls = list(zip(attrib, data))
-        df = pd.DataFrame(ls)
-        return df
-        
-
-# Handle all methods related to stock
-class Stock:
-    def __init__(self, stock_cd):
-        self.stock_cd = stock_cd
-        print(f"Stock Cd: {self.stock_cd}")
-        self.overviewp = Webpage.from_url(f"https://www.investing.com/equities/{stock_cd}")
-        self.stock_id = self.get_id()
-        print(f"Stock Id: {self.stock_id}")
-        self.ratiosp = Webpage.from_url(f"https://www.investing.com/equities/{stock_cd}-ratios")
-        self.cash_flowp = Webpage.from_url(f"https://www.investing.com/instruments/Financials/changereporttypeajax?action=change_report_type&pair_ID={self.stock_id}&report_type=CAS&period_type=Annual")
-        self.balance_sheetp = Webpage.from_url(f"https://www.investing.com/instruments/Financials/changereporttypeajax?action=change_report_type&pair_ID={self.stock_id}&report_type=BAL&period_type=Annual")
-        # self.income_statementp = Webpage.from_url(f"https://www.investing.com/equities/{stock_cd}-income-statement")
-        # self.earningsp = Webpage.from_url(f"https://www.investing.com/equities/{stock_cd}-earnings")
-        # self.financialp = Webpage.from_url(f"https://www.investing.com/equities/{stock_cd}-financial-summary")
-    
-    def get_growth_rate(self):
-        """scrape growth rate from simply wall st"""
-
-        # search the link for stock
-        stock_cd = self.stock_cd.replace("-", " ")
-        params = (
-            ('x-algolia-agent', 'Algolia for JavaScript (4.2.0); Browser (lite)'),
-            ('x-algolia-api-key', 'be7c37718f927d0137a88a11b69ae419'),
-            ('x-algolia-application-id', '17IQHZWXZW'),
-        )
-        data = f'{{"query":"{stock_cd} klse","highlightPostTag":" ","highlightPreTag":" ","restrictHighlightAndSnippetArrays":true}}'
-        try:
-            response = requests.post('https://17iqhzwxzw-dsn.algolia.net/1/indexes/companies/query', params=params, data=data)
-
-            # generate link
-            stock_url = response.json()['hits'][0]['url']
-            url = "https://simplywall.st" + stock_url
-        except:
-            return None
-        
-        html = url2html(url)
-        soup = BeautifulSoup(html, 'html.parser')
-        growth = soup.find('p', {'data-cy-id': 'key-metric-value-forecasted-annual-earnings-growth'}).get_text().replace('%', '')
-        self.growth_rate = float(growth)/100
-        print(f"Growth Rate: {self.growth_rate}")
-        return [self.growth_rate, url]
-    
-    def get_beta(self):
-        """scrape beta from infrontanalytics.com"""
-
-        # search the link for stock
-        params = (
-            ('keyname', self.stock_cd.replace("-", " ")),
-        )
-        response = requests.get('https://www.infrontanalytics.com/Eurofin/autocomplete', params=params, verify=False)
-        result = response.json()[0]
-
-        # generate stock url
-        name = result['name'].replace(" ", "-").replace(".", "") + "-"
-        code = result['isin']
-        url = f"https://www.infrontanalytics.com/fe-en/{code}/{name}/beta"
-
-        # get beta
-        html = url2html(url)
-        m = re.search(r"shows a Beta of ([+-]?\d+\.\d+).", html)
-        self.beta = m.groups()[0]
-        print(f"Beta: {self.beta}")
-        return float(self.beta), url
-    
-    def get_discount_rate(self):
-        """convert beta to discount rate for dcf model"""
-
-        beta, url = self.get_beta()
-                
-        dr = {0.8: 5, 
-        1: 6, 
-        1.1: 6.8, 
-        1.2: 7, 
-        1.3: 7.9, 
-        1.4: 8, 
-        1.5: 8.9}
-        for key in dr:
-            if beta < key:
-                discount_rate = dr[key]
-            else:
-                discount_rate = 9
-        self.discount_rate = round(discount_rate/100, 2)
-        print(f"Discount Rate: {self.discount_rate}")
-        return [self.discount_rate, url]
-        
-    def get_id(self):
-        m = re.search('data-pair-id="(\d+)"', self.overviewp.html)
-        stock_id = m.groups()[0]
-        return stock_id
-    
-    def overview(self):
-        soup = self.overviewp.soup
-        last_price = soup.find('span', {'id':'last_last'}).get_text()
-        ls = ['Last Price', last_price]
-        df = pd.DataFrame([ls])
-        
-        overview = self.overviewp.get_span('span', ['float_lang_base_1', 'float_lang_base_2'])
-        return pd.concat([df, overview])
-    
-    def ratios(self):
-        tables = self.ratiosp.tables
-        numbers = range(1, 9)
-        ratios = pd.concat(tables[i] for i in numbers)
-        return ratios
-    
-    def cash_flow(self):
-        df = self.cash_flowp.tables[0]
-        cash_flow = df[~df[1].str.contains("a|e|i|o|u")]
-        return cash_flow
-    
-    def balance_sheet(self):
-        df = self.balance_sheetp.tables[0]
-        balance_sheet = df[~df[1].str.contains("a|e|i|o|u")]
-        return balance_sheet
-    
-    def earnings(self):
-        s = requests.Session()
-        url = f"https://www.investing.com/equities/{self.stock_cd}-earnings"
-        headers={ "User-Agent": "Mozilla/5.0"}
-        r = s.get(url, headers={ "User-Agent": "Mozilla/5.0"})
-        
-        # get more history - to work on
-        '''
-        more_history = "https://www.investing.com/equities/morehistory"
-        headers = {
-            'User-Agent': 'Mozilla/5.0',
-            'X-Requested-With': 'XMLHttpRequest',
-            'Referer': url,
-        }
-        data = {"pairID" : "41688", "last_timestamp": "2019-0-02"}
-        r = s.post(more_history, headers=headers, cookies=r.cookies, data=data)
-        r.json()['historyRows']
-        '''
-        return r.text
-    
-    def financial_summary(self):
-        def get_summary(html):
-            webpage = Webpage(html)
-            soup = webpage.soup
-
-            title = soup.find('h3').text
-            df = webpage.get_span('span', ['float_lang_base_1', 'float_lang_base_2'])
-            table = pd.read_html(str(soup))[0]
-            return [title, table, df] # pd.concat([table, df], axis=0, ignore_index=True)
-        
-        financial_summary = f"https://www.investing.com/instruments/Financials/changesummaryreporttypeajax?action=change_report_type&pid={stock_id}&financial_id={stock_id}&ratios_id={stock_id}&period_type="
-        annual = financial_summary + "Annual"
-        # interim = financial_summary + "Interim"
-        
-        df = pd.DataFrame()
-        soup = Webpage.from_url(annual).soup
-        sections = soup.find_all('div', "companySummaryIncomeStatement")
-        result = []
-        for i in sections:
-            result.append(get_summary(str(i)))
-        return result
-            
-'''
-10% for public companies
-15% for private companies that are scaling predictably (say above $10m in ARR, and growing greater than 40% year on year)
-20% for private companies that have not yet reached scale and predictable growth
-'''
-
 def rename_excel(my_stock, excel_name):
     """rename excel sheet with npv and last price for easy viewing"""
 
-    operating_cf = search("Cash From Operating Activities", my_stock.cash_flow())
-    shares_outstanding = search("Shares Outstanding", my_stock.overview())/1000000
-    last_price = search("Last Price", my_stock.overview())
+    operating_cf = search("Cash From Operating Activities", my_stock.cash_flow)
+    shares_outstanding = search("Shares Outstanding", my_stock.overview)/1000000
+    last_price = search("Last Price", my_stock.overview)
 
     cash_flow = []
     for i in range(1, 11):
@@ -275,11 +71,11 @@ def analyse(company_name):
     # Stock and write to excel
     table1 = {
         "Name of Stock": my_stock.stock_cd.replace("-", " ").title(),
-        "Operating Cash Flow": search("Cash From Operating Activities", my_stock.cash_flow()),
-        "Total Debt": search("Total Long Term Debt", my_stock.balance_sheet()),
-        "Cash & Equivalent": search("Cash & Equivalent", my_stock.balance_sheet()),
+        "Operating Cash Flow": search("Cash From Operating Activities", my_stock.cash_flow),
+        "Total Debt": search("Total Long Term Debt", my_stock.balance_sheet),
+        "Cash & Equivalent": search("Cash & Equivalent", my_stock.balance_sheet),
         "Growth Rate": 0,
-        "No. of Shares Outstanding": search("Shares Outstanding", my_stock.overview())/1000000,
+        "No. of Shares Outstanding": search("Shares Outstanding", my_stock.overview)/1000000,
         "Discount Rate": 0
     }
 
@@ -289,8 +85,8 @@ def analyse(company_name):
     worksheet.write_column('B1', table1.values(), colored_format)
 
     # rewrite in percentage
-    worksheet.write('B5', my_stock.get_growth_rate()[0], percentage_format)
-    worksheet.write('B7', my_stock.get_discount_rate()[0], percentage_format)
+    worksheet.write('B5', my_stock.growth_rate, percentage_format)
+    worksheet.write('B7', my_stock.discount_rate, percentage_format)
 
 
 
@@ -329,13 +125,13 @@ def analyse(company_name):
 
 
     # Stock overview, table 3
-    df = my_stock.overview().reset_index(drop=True)
+    df = my_stock.overview.reset_index(drop=True)
     index = [0, 5, 6, 7, 8, 9, 11, 15]
     worksheet.write_column('G2', df.iloc[index, 0])
     worksheet.write_column('H2', df.iloc[index, 1])
 
     # report
-    worksheet.write_column('A22', [my_stock.get_growth_rate()[1], my_stock.get_beta()[1]])
+    worksheet.write_column('A22', [my_stock.growth_rate, my_stock.beta])
 
     workbook.close()
     # Shift + Ctrl + F9
@@ -344,20 +140,10 @@ def analyse(company_name):
 
 
 def main():
-
     # print('Number of arguments: {}'.format(len(sys.argv[1:])))
     # print('Argument(s) passed: {}'.format(str(sys.argv[1:])))
 
     companies = sys.argv[1:]
-
     list(map(lambda x: analyse(x),companies))
-
-
-
-
-
-
-
-
   
 main()
---
+++ stock.py
@@ -0,0 +1,231 @@
+import requests
+import pandas as pd
+import re
+from bs4 import BeautifulSoup
+
+def url2html(url, headers=None, params=None, data=None):
+    headers={'User-Agent': 'Mozilla/5.0'}
+    try:
+        req = requests.get(url, headers=headers, params=params, data=data)
+    except:
+        req = requests.get(url, headers=headers, params=params, data=data, verify=False)
+    html = req.text
+    return html
+
+    
+# Handle all urls and htmls
+class Webpage:
+    def __init__(self, html):
+        self.html = html
+        self.soup = BeautifulSoup(self.html, 'html.parser')
+        try:
+            self.tables = pd.read_html(self.html)
+        except:
+            self.tables = None
+    
+    @classmethod
+    def from_url(cls, url, headers=None, params=None, data=None):
+        """constructor with url"""
+
+        html = url2html(url, headers)
+        return cls(html)
+    
+    def get_span(self, tag: str, class_name: list):
+        """return df from columns not in <table>"""
+        
+        def get_tag(tag, class_name):
+            tags = self.soup.find_all(tag, {'class': class_name})
+            text = [i.get_text() for i in tags if i.get_text() != '']
+            return text
+        
+        attrib = get_tag(tag, class_name[0])
+        data = get_tag(tag, class_name[1])
+
+        ls = list(zip(attrib, data))
+        df = pd.DataFrame(ls)
+        return df
+
+
+
+# Handle all methods related to stock
+class Stock:
+    def __init__(self, stock_cd):
+        self.stock_cd = stock_cd
+        print(f"Stock Cd: {self.stock_cd}")
+
+        self.overview, self.stock_id = self.scrape_overview()
+        print(f"Stock Id: {self.stock_id}")
+
+        self.growth_rate, self.growth_rate_url = self.scrape_growth_rate()
+        self.beta, self.beta_url = self.scrape_beta()
+        self.discount_rate = self.scrape_discount_rate()
+
+        self.ratios = self.scrape_ratios()
+        self.cash_flow = self.scrape_cash_flow()
+        self.balance_sheet = self.scrape_balance_sheet()
+
+
+        # self.income_statementp = Webpage.from_url(f"https://www.investing.com/equities/{stock_cd}-income-statement")
+        # self.earningsp = Webpage.from_url(f"https://www.investing.com/equities/{stock_cd}-earnings")
+        # self.financialp = Webpage.from_url(f"https://www.investing.com/equities/{stock_cd}-financial-summary")
+    
+    def scrape_growth_rate(self):
+        """scrape growth rate from simply wall st"""
+
+        # search the link for stock
+        stock_cd = self.stock_cd.replace("-", " ")
+        params = (
+            ('x-algolia-agent', 'Algolia for JavaScript (4.2.0); Browser (lite)'),
+            ('x-algolia-api-key', 'be7c37718f927d0137a88a11b69ae419'),
+            ('x-algolia-application-id', '17IQHZWXZW'),
+        )
+        data = f'{{"query":"{stock_cd} klse","highlightPostTag":" ","highlightPreTag":" ","restrictHighlightAndSnippetArrays":true}}'
+        try:
+            response = requests.post('https://17iqhzwxzw-dsn.algolia.net/1/indexes/companies/query', params=params, data=data)
+
+            # generate link
+            stock_url = response.json()['hits'][0]['url']
+            url = "https://simplywall.st" + stock_url
+        except:
+            return None
+        
+        html = url2html(url)
+        soup = BeautifulSoup(html, 'html.parser')
+        growth = soup.find('p', {'data-cy-id': 'key-metric-value-forecasted-annual-earnings-growth'}).get_text().replace('%', '')
+        self.growth_rate = float(growth)/100
+        print(f"Growth Rate: {self.growth_rate}")
+        return self.growth_rate, url
+    
+    def scrape_beta(self):
+        """scrape beta from infrontanalytics.com"""
+
+        # search the link for stock
+        params = (
+            ('keyname', self.stock_cd.replace("-", " ")),
+        )
+        response = requests.get('https://www.infrontanalytics.com/Eurofin/autocomplete', params=params, verify=False)
+        result = response.json()[0]
+
+        # generate stock url
+        name = result['name'].replace(" ", "-").replace(".", "") + "-"
+        code = result['isin']
+        url = f"https://www.infrontanalytics.com/fe-en/{code}/{name}/beta"
+
+        # get beta
+        html = url2html(url)
+        m = re.search(r"shows a Beta of ([+-]?\d+\.\d+).", html)
+        beta = m.groups()[0]
+        print(f"Beta: {beta}")
+        return float(beta), url
+    
+    def scrape_discount_rate(self):
+        """convert beta to discount rate for dcf model"""
+        
+        discount_rate = 0
+        dr = {0.8: 5, 
+        1: 6, 
+        1.1: 6.8, 
+        1.2: 7, 
+        1.3: 7.9, 
+        1.4: 8, 
+        1.5: 8.9}
+        for key in dr:
+            if self.beta < key:
+                discount_rate = dr[key]
+            else:
+                discount_rate = 9
+        discount_rate = round(discount_rate/100, 2)
+        print(f"Discount Rate: {discount_rate}")
+        return discount_rate
+        
+
+    
+    def scrape_overview(self):
+        stock_cd = self.stock_cd
+        def scrape_id(overviewp):
+            m = re.search('data-pair-id="(\d+)"', overviewp.html)
+            stock_id = m.groups()[0]
+            return stock_id
+
+        overviewp = Webpage.from_url(f"https://www.investing.com/equities/{stock_cd}")
+        soup = overviewp.soup
+        last_price = soup.find('span', {'id':'last_last'}).get_text()
+        ls = ['Last Price', last_price]
+        df = pd.DataFrame([ls])
+        
+        overview = overviewp.get_span('span', ['float_lang_base_1', 'float_lang_base_2'])
+        stock_id = scrape_id(overviewp)
+
+        return pd.concat([df, overview]), stock_id
+    
+    def scrape_ratios(self):
+        stock_cd = self.stock_cd
+        ratiosp = Webpage.from_url(f"https://www.investing.com/equities/{stock_cd}-ratios")
+        tables = ratiosp.tables
+        numbers = range(1, 9)
+        ratios = pd.concat(tables[i] for i in numbers)
+        return ratios
+    
+    def scrape_cash_flow(self):
+        stock_id = self.stock_id
+        cash_flowp = Webpage.from_url(f"https://www.investing.com/instruments/Financials/changereporttypeajax?action=change_report_type&pair_ID={self.stock_id}&report_type=CAS&period_type=Annual")
+        df = cash_flowp.tables[0]
+        cash_flow = df[~df[1].str.contains("a|e|i|o|u")]
+        return cash_flow
+    
+    def scrape_balance_sheet(self):
+        stock_id = self.stock_id
+        balance_sheetp = Webpage.from_url(f"https://www.investing.com/instruments/Financials/changereporttypeajax?action=change_report_type&pair_ID={self.stock_id}&report_type=BAL&period_type=Annual")
+        df = balance_sheetp.tables[0]
+        balance_sheet = df[~df[1].str.contains("a|e|i|o|u")]
+        return balance_sheet
+    
+    def scrape_earnings(self):
+        stock_cd = self.stock_cd
+        s = requests.Session()
+        url = f"https://www.investing.com/equities/{self.stock_cd}-earnings"
+        headers={ "User-Agent": "Mozilla/5.0"}
+        r = s.get(url, headers={ "User-Agent": "Mozilla/5.0"})
+        
+        # get more history - to work on
+        '''
+        more_history = "https://www.investing.com/equities/morehistory"
+        headers = {
+            'User-Agent': 'Mozilla/5.0',
+            'X-Requested-With': 'XMLHttpRequest',
+            'Referer': url,
+        }
+        data = {"pairID" : "41688", "last_timestamp": "2019-0-02"}
+        r = s.post(more_history, headers=headers, cookies=r.cookies, data=data)
+        r.json()['historyRows']
+        '''
+        return r.text
+    
+    def scrape_financial_summary(self):
+        def get_summary(html):
+            webpage = Webpage(html)
+            soup = webpage.soup
+
+            title = soup.find('h3').text
+            df = webpage.get_span('span', ['float_lang_base_1', 'float_lang_base_2'])
+            table = pd.read_html(str(soup))[0]
+            return [title, table, df] # pd.concat([table, df], axis=0, ignore_index=True)
+        
+        stock_id = self.stock_id
+        financial_summary = f"https://www.investing.com/instruments/Financials/changesummaryreporttypeajax?action=change_report_type&pid={stock_id}&financial_id={stock_id}&ratios_id={stock_id}&period_type="
+        annual = financial_summary + "Annual"
+        # interim = financial_summary + "Interim"
+        
+        df = pd.DataFrame()
+        soup = Webpage.from_url(annual).soup
+        sections = soup.find_all('div', "companySummaryIncomeStatement")
+        result = []
+        for i in sections:
+            result.append(get_summary(str(i)))
+        return result
+            
+'''
+10% for public companies
+15% for private companies that are scaling predictably (say above $10m in ARR, and growing greater than 40% year on year)
+20% for private companies that have not yet reached scale and predictable growth
+'''
